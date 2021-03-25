4964f054a01317c0c67be7688170af044513b163
Kendra <kendratay@yahoo.com>
2021-03-25 15:04:59 -0500

    Initial commit

---
+++ src/main/java/com/tts/TechTalentTwitter/TechTalenttwitterApplication.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalenttwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalenttwitterApplication.class, args);
+	}
+
+}
